0153d090e923355fd9a3ecccf5f3950114d9c057
LeonardoTozzi <leonardotozzis@gmail.com>
2021-09-17 11:01:10 -0300

    Estudo

---
+++ Aula 14/ambiente.js
@@ -0,0 +1,13 @@
+/*var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+//--------------
+
+console.log('Vai comceçar')
+for(var c = 1;c <= 5; c++){
+    console.log(c) // É a mesma função que o conjunto de cima.
+}
+console.log('FIM')
